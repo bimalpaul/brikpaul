e2ab55057570d1784ac7e26ed66bf103e5b94016
Bimal Paul <bimal.paul@autodesk.com>
2022-02-08 15:02:14 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import brik from './brik.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={brik} className="App-logo" alt="logo" />
+        <p>
+          Come play! 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
